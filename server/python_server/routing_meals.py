51519f287216447bf57d51d9b64c92800eb1bb02
Raul Catena <raulcatena@gmail.com>
2017-09-16 12:44:17 +0200

    Test calls to API

--- server/python_server/routing_meals.py
+++ server/python_server/routing_meals.py
@@ -1,10 +1,11 @@
+from app import app
 import os,sys,inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir) 
 
-import speech_analysis
+from speech_analysis import SpeechAnalysis
 
-@app.route('/testSpeech', methods=['POST'])
-def signUp():
-    return functions_accounts.signUp()
+@app.route('/testSpeech', methods=['GET'])
+def testSpeech():
+    return SpeechAnalysis.parse_foods('../data/chicken_dinner.mp3')
