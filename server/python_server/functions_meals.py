5170acb2f477929f8535fd6f750c25dd93f4bfb3
Raul Catena <raulcatena@gmail.com>
2017-09-16 15:27:08 +0200

    Refactored Meal functions

---
+++ server/python_server/functions_meals.py
@@ -0,0 +1,21 @@
+from werkzeug import generate_password_hash, check_password_hash
+from flask import render_template, make_response, request, json, jsonify, flash
+#https://pythonhosted.org/flask-mail/
+#from mailer import sendMail
+from app import app_secret, db
+from access_control import userRequired
+
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+
+from speech_analysis import SpeechAnalysis
+
+
+@userRequired
+def testMeals():
+    _token = request.form['token']
+    _audio_file = request.form['audio_file']
+    return SpeechAnalysis.parse_foods('../data/chicken_dinner.mp3')
+    
--- server/python_server/routing_meals.py
+++ server/python_server/routing_meals.py
@@ -1,11 +1,7 @@
 from app import app
-import os,sys,inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir) 
 
-from speech_analysis import SpeechAnalysis
+import functions_meals
 
 @app.route('/testSpeech', methods=['GET'])
 def testSpeech():
-    return SpeechAnalysis.parse_foods('../data/chicken_dinner.mp3')
+    return functions_meals.testMeals()
